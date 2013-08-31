096a5ac9ac21e8c3ac79fe6184e8772f82df182a
OliverG96 <oliverarafo@gmail.com>
2013-08-31 19:10:47 +0100

    board: Move wifi defines to u8500-common

--- BoardCommonConfig.mk
+++ BoardCommonConfig.mk
@@ -33,13 +33,6 @@ BOARD_KERNEL_PAGESIZE := 2048
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/golden-common/bluetooth
 BOARD_BLUEDROID_VENDOR_CONF := device/samsung/golden-common/bluetooth/btvendor_golden.txt
 
-# Wifi
-WIFI_DRIVER_FW_PATH_STA := "/system/etc/wifi/bcmdhd_sta.bin"
-WIFI_DRIVER_FW_PATH_AP := "/system/etc/wifi/bcmdhd_apsta.bin"
-WIFI_DRIVER_FW_PATH_P2P := "/system/etc/wifi/bcmdhd_p2p.bin"
-WIFI_DRIVER_MODULE_ARG := "firmware_path=/system/etc/wifi/bcmdhd_sta.bin nvram_path=/system/etc/wifi/nvram_net.txt"
-WIFI_DRIVER_MODULE_AP_ARG := "firmware_path=/system/etc/wifi/bcmdhd_apsta.bin nvram_path=/system/etc/wifi/nvram_net.txt"
-
 # Vold
 BOARD_VOLD_MAX_PARTITIONS := 25
 BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
